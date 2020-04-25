0dbfb6edb9f7cf4313cd1075352536106ceec49a
Abhinav Mehta <abhinav.mehta0208@gmail.com>
2020-04-25 15:55:47 -0500

    Change train data type to Eigen Vector

--- main.cpp
+++ main.cpp
@@ -5,42 +5,63 @@
 ////////////////////////////////////////////////////////
 
 #include <iostream>
-#include <Eigen/Dense>
+#include <Eigen/Core>
 #include <opencv2/opencv.hpp>
 #include <vector>
 
 using namespace std;
 
-vector<int> getTokens(string line, char delimiter){
+// Utility function to tokenize a line for given delimiter
+vector<double> getTokens(string line, char delimiter){
 	stringstream lineStream(line);
-	vector<int> tokens;
+	vector<double> tokens;
 	string word;
 	
 	// traverse the line stream using delimiter
 	while(getline(lineStream, word, delimiter)){
-		if(delimiter!=' ' || word!="") tokens.push_back(stoi(word));
+		if(delimiter!=' ' || word!="") tokens.push_back((double)stoi(word));
 	}
 
 	// return tokens vector
 	return tokens;
 }
 
-vector<vector<int>> read_data(string filename){
-	vector<vector<int>> result;
+// Utility function to read csv file
+void read_data(string filename, vector<vector<double>>& data, vector<int>& labels){
 	ifstream inputFile(filename);
 	if(inputFile.is_open()){
 		string line;
 		while(getline(inputFile, line)){
-			vector<int> tokens = getTokens(line, ',');
-			result.push_back(tokens);
+			vector<double> tokens = getTokens(line, ',');
+			labels.push_back((int)tokens.back());
+			tokens.pop_back();
+			data.push_back(tokens);
 		}
 	}
-	return result;
+}
+
+// Utility function to convert data to Eigen Vectors
+vector<Eigen::VectorXd> convert_data(vector<vector<double>> vector_data){
+	vector<Eigen::VectorXd> eigen_data;
+	for(int i=0;i<vector_data.size();i++){
+		Eigen::VectorXd temp = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(vector_data[i].data(), vector_data[i].size());
+		eigen_data.push_back(temp);
+	}
+	return eigen_data;
 }
 
 int main()
 {
-	vector<vector<int>> train_data = read_data("./data/train.csv");
-	vector<vector<int>> test_data = read_data("./data/test.csv");
+	vector<vector<double>> im_train;
+	vector<int> im_labels;
+	read_data("./data/train.csv", im_train, im_labels);
+	vector<Eigen::VectorXd> train_data = convert_data(im_train);
+
+	vector<vector<double>> im_test;
+	vector<int> im_test_labels;
+	read_data("./data/test.csv", im_test, im_test_labels);
+	vector<Eigen::VectorXd> test_data = convert_data(im_test);
+
+
 	return 0;
 }
