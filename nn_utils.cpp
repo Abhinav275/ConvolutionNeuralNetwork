09a62694021d47883a7aa705855c3a62a85bb6c5
Abhinav Mehta <abhinav.mehta0208@gmail.com>
2020-05-12 11:15:50 -0500

    Add padding and single convolution

--- nn_utils.cpp
+++ nn_utils.cpp
@@ -98,16 +98,79 @@ Eigen::VectorXd relu_backwards(Eigen::VectorXd dl_dy, Eigen::VectorXd x){
 	return dl_dy;
 }
 
-// function to convolve over given image
-Eigen::Tensor conv(Eigen::MatrixXd img, Eigen::Tensor<double, 4> w_conv, Eigen::MatrixXd b_conv){
+// function to pad an image with zeros
+Eigen::MatrixXd pad(Eigen::MatrixXd img){
+	Eigen::MatrixXd x = Eigen::MatrixXd::Zero(img.rows()+2, img.cols()+2);
+	for(int i=1;i<img.rows()+1;i++){
+		for(int j=1;j<img.cols()+1;j++){
+			x(i,j) = img(i-1, j-1);
+		}
+	}
+	return x;
+}
+
+template<typename T>
+using  MatrixType = Eigen::Matrix<T,Eigen::Dynamic, Eigen::Dynamic>;
+
+// function to convert matrix to tensor
+template<typename Scalar, typename... Dims>
+auto Matrix_to_Tensor(const MatrixType<Scalar> &matrix, Dims... dims)
+{
+    constexpr int rank = sizeof... (Dims);
+    return Eigen::TensorMap<Eigen::Tensor<const Scalar, rank>>(matrix.data(), {dims...});
+}
+
+// function to convert tensor to matrix
+template<typename Scalar,int rank, typename sizeType>
+auto Tensor_to_Matrix(const Eigen::Tensor<Scalar,rank> &tensor,const sizeType rows,const sizeType cols)
+{
+    return Eigen::Map<const MatrixType<Scalar>> (tensor.data(), rows,cols);
+}
+
+
+
+// function to convolute over given image
+Eigen::Tensor<double, 3> conv(Eigen::MatrixXd img, Eigen::Tensor<double, 4> w_conv, Eigen::MatrixXd b_conv){
 	Eigen::Tensor<double, 3> y(img.rows(), img.cols(), w_conv.dimension(3));
 	y.setZero();
 
+	img = pad(img);
+	Eigen::Tensor<double, 2> img_tensor = Matrix_to_Tensor(img, img.rows(), img.cols()); 
+	Eigen::array<int, 4> w_extents = {w_conv.dimension(0), w_conv.dimension(1), 1, 1};
+	Eigen::array<int, 2> img_extents = {w_conv.dimension(0), w_conv.dimension(1)};
+
+	cout<<img_tensor<<endl;
+	cout<<w_conv<<endl;
+	Eigen::array<int, 2> offsets = {0, 0};
+	Eigen::Tensor<double, 2> sub_image = img_tensor.slice(offsets, img_extents);
+
+	Eigen::MatrixXd sub_image_matrix = Tensor_to_Matrix(sub_image, 3, 3);
 	
+	Eigen::array<int, 4> w_offsets = {0, 0, 0, 0};
+	Eigen::Tensor<double, 4> filter = w_conv.slice(w_offsets, w_extents);
+
+	Eigen::MatrixXd filter_matrix = Tensor_to_Matrix(filter, 3, 3);
+
+	Eigen::MatrixXd mul = (sub_image_matrix.array() * filter_matrix.array()).matrix();
+	cout<<sub_image(0,0)<<endl;
+	cout<<filter(0,0,0,0)<<endl;
+	cout<<mul<<endl;
+	cout<<mul.sum()<<endl;
+	// for(int i=0;i<img_tensor.dimension(0)-w_conv.dimension(0)+1;i++){
+	// 	for(int j=0;j<img_tensor.dimension(1)-w_conv.dimension(1)+1;j++){
+			
+	// 	}
+	// }
+	// for(int i=0;i<w_conv.dimension(3);i++){
+	// 	Eigen::array<int, 4> offsets = {0, 0, 0, i};
+	// 	auto filter = w_conv.slice(offsets, extents);
+	// }
+
+	return y;
 }
 
 // function to get relu of given of convolution result
-Eigen::Tensor relu_conv(Eigen::Tensor x){
+Eigen::Tensor<double, 3> relu_conv(Eigen::Tensor<double, 3> x){
 	for(int i=0;i<x.dimension(0);i++){
 		for(int j=0;j<x.dimension(1);j++){
 			for(int k=0;k<x.dimension(2);k++){
@@ -120,7 +183,7 @@ Eigen::Tensor relu_conv(Eigen::Tensor x){
 
 
 // function to get relu of given of convolution result
-Eigen::Tensor relu__conv_backward(Eigen::Tensor dl_dy, Eigen::Tensor x, Eigen::Tensor y_pred){
+Eigen::Tensor<double, 3> relu__conv_backward(Eigen::Tensor<double, 3> dl_dy, Eigen::Tensor<double, 3> x, Eigen::Tensor<double, 3> y_pred){
 	for(int i=0;i<x.dimension(0);i++){
 		for(int j=0;j<x.dimension(1);j++){
 			for(int k=0;k<x.dimension(2);k++){
@@ -132,7 +195,7 @@ Eigen::Tensor relu__conv_backward(Eigen::Tensor dl_dy, Eigen::Tensor x, Eigen::T
 }
 
 // function to get 2x2 max pooling results for matrix
-Eigen::Tensor pool2x2(Eigen::MatrixXd x){
+Eigen::Tensor<double, 3> pool2x2(Eigen::Tensor<double, 3> x){
 	Eigen::Tensor<double, 3> y(x.dimension(0)/2, x.dimension(1)/2, x.dimension(3));
 	y.setZero();
 	int a = 0;
@@ -151,7 +214,7 @@ Eigen::Tensor pool2x2(Eigen::MatrixXd x){
 }
 
 // function to get 2x2 max pooling backward
-Eigen::Tensor pool2x2_backward(Eigen::Tensor dl_dy, Eigen::Tensor x, Eigen::Tensor y){
+Eigen::Tensor<double, 3> pool2x2_backward(Eigen::Tensor<double, 3> dl_dy, Eigen::Tensor<double, 3> x, Eigen::Tensor<double, 3> y){
 	Eigen::Tensor<double, 3> result(x.dimension(0), x.dimension(1), x.dimension(3));
 	result.setZero();
 	int a = 0;
